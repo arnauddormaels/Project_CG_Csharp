be7143e501eca4b10477543c3ade9f03ebeb58da
JuanEstebanPradaEspinosa <juan.pradaespinosa@student.hogent.be>
2023-10-31 12:15:42 +0100

    consturctor toegevoegd aan recipe model

--- CollectAndGo.Persistence/Model/Recipe.cs
+++ CollectAndGo.Persistence/Model/Recipe.cs
@@ -8,6 +8,15 @@ namespace CG.Persistence.Model
 {
     public class Recipe
     {
+        public Recipe(string name, string category, bool active, string imgUrl, string videoUrl)
+        {
+            Name = name;
+            Category = category;
+            Active = active;
+            ImgUrl = imgUrl;
+            VideoUrl = videoUrl;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }

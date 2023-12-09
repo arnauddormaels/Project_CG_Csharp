3fec92b53c946245fb60b8e7865a0006124afbc5
samcasters <sam.casters@gmail.com>
2023-12-09 20:53:58 +0100

    Product exceptions

--- CG.BL/Models/Product.cs
+++ CG.BL/Models/Product.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CG.BL.Exceptions;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,11 +31,75 @@ namespace CG.BL.Models
             BrandProduct = brandProduct;
         }
 
-        public string Category { get => _category; private set => _category = value; }
-        public string ProductName { get => _productName; private set => _productName = value; }
-        public int ProductId { get => _productId; set => _productId = value; }
-        public string ImgUrl { get => _imgUrl; set => _imgUrl = value; }
-        public BrandProduct BrandProduct { get => _brandProduct; set => _brandProduct = value; }
+        public int ProductId
+        {
+            get => _productId; private set
+            {
+                if (value <= 0)
+                {
+                    var ex = new DomainModelException("Product-SetProductId-SmallerThanOne");
+                    ex.Sources.Add(new ErrorSource(this.GetType().Name, nameof(ProductId)));
+                    ex.Error = new Error("productId is smaller than 1");
+                    ex.Error.Values.Add(new PropertyInfo("ProductId", value));
+                    throw ex;
+                }
+                ProductId = value;
+            }
+        }
+        public string ProductName { get => _productName; private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    var ex = new DomainModelException("Product-SetProductName-NullOrWhiteSpace");
+                    ex.Sources.Add(new ErrorSource(this.GetType().Name, nameof(ProductName)));
+                    ex.Error = new Error("productName null or white space");
+                    ex.Error.Values.Add(new PropertyInfo("ProductName", value));
+                    throw ex;
+                }
+                ProductName = value;
+            }
+        }
+        public string Category
+        {
+            get => _category; private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    var ex = new DomainModelException("Product-SetCategory-NullOrWhiteSpace");
+                    ex.Sources.Add(new ErrorSource(this.GetType().Name, nameof(Category)));
+                    ex.Error = new Error("category null or white space");
+                    ex.Error.Values.Add(new PropertyInfo("Category", value));
+                    throw ex;
+                }
+                Category = value;
+            }
+        }
+        public string ImgUrl { get => _imgUrl; private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    var ex = new DomainModelException("Product-SetImgUrl-NullOrWhiteSpace");
+                    ex.Sources.Add(new ErrorSource(this.GetType().Name, nameof(ImgUrl)));
+                    ex.Error = new Error("imgUrl is null or white space");
+                    ex.Error.Values.Add(new PropertyInfo("ImgUrl", value));
+                    throw ex;
+                }
+                ImgUrl = value;
+            }
+        }
+        public BrandProduct BrandProduct { get => _brandProduct; private set
+            {
+                if (BrandProduct == null)
+                {
+                    var ex = new DomainModelException("Product-SetBrandProduct-Null");
+                    ex.Sources.Add(new ErrorSource(this.GetType().Name, nameof(BrandProduct)));
+                    ex.Error = new Error("brandProduct is null ");
+                    ex.Error.Values.Add(new PropertyInfo("BrandProduct", value));
+                    throw ex;
+                }
+                BrandProduct = value;
+            }
+        }
 
         public override string ToString()
         {
